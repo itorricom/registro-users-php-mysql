a13ccaefd701019219c8ddaa5b8cc0626d2bb41b
Roberto Murillo <remd80@gmail.com>
2018-08-23 03:34:11 -0600

    Actualizando las definiciones de la base de datos

--- archivos_php/conexion.php
+++ archivos_php/conexion.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_SERVER', 'nombre_del_servidor');
 define('DB_USER', 'nombre_del_usuario');
-define('DB_PASSWD', 'contraseña_del_usuario');
+define('DB_PASSWD', 'contraseña_de_la_base_de_datos');
 define('DB_NAME', 'nombre_de_la_base_de_datos');
 
 $conexion = mysqli_connect(DB_SERVER, DB_USER, DB_PASSWD, DB_NAME) or die('No se pudo conectarse con la Base de Datos');
