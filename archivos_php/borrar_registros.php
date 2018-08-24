e6cde321665ab555e0f8bd274c0c85b6cec37166
Roberto Murillo <remd80@gmail.com>
2018-08-24 10:03:13 -0600

    Aplicando seguridad al ID del GET

--- archivos_php/borrar_registros.php
+++ archivos_php/borrar_registros.php
@@ -3,7 +3,8 @@ require_once("conexion.php");
 
 $id = $_GET['id'];
 
-$solicitud = "DELETE FROM datos_usuarios WHERE ID=$id";
-$resultado = mysqli_query($conexion, $solicitud);
+$seg_id = mysqli_real_escape_string($conexion, $id);
+$borrar = "DELETE FROM datos_usuarios WHERE ID={$seg_id}";
+$resultado = mysqli_query($conexion, $borrar);
 header("location: ../index.php");
 ?>
